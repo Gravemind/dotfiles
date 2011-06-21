9e21a70373089dc05eb55a3f66ab68031f5a2d66
Gravemind <gravemind2a@gmail.com>
2011-06-21 17:16:22 +0200

    *oh-my-zsh change less colors

--- .oh-my-zsh/custom/env.zsh
+++ .oh-my-zsh/custom/env.zsh
@@ -4,13 +4,13 @@ then
     export TERM="rxvt-256color"
 fi
 
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_mb=$'\E[01;34m'
+export LESS_TERMCAP_md=$'\E[01;34m'
 export LESS_TERMCAP_me=$'\E[0m'
 export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_so=$'\E[0m\E[1m'
 export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[01;32m'
+export LESS_TERMCAP_us=$'\E[01;36m'
 
 export EDITOR='emacs'
 export PAGER='less'
