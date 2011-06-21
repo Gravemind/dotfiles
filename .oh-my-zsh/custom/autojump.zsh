f71535b5323a4362d345ba497fc0ded53ff28814
Gravemind <gravemind2a@gmail.com>
2011-06-21 13:42:15 +0200

    *oh-my-zsh/autojump

--- .oh-my-zsh/custom/autojump.zsh
+++ .oh-my-zsh/custom/autojump.zsh
@@ -1,2 +1,11 @@
 
-source /etc/profile.d/autojump.zsh
+AUTOJUMP_FILES=( '/etc/profile.d/autojump.zsh' '/usr/share/autojump/autojump.zsh' )
+
+for AUTOJUMP_FILE in $AUTOJUMP_FILES
+do
+    if [ -f "$AUTOJUMP_FILE" ]
+    then
+        source "$AUTOJUMP_FILE"
+        break;
+    fi
+done
