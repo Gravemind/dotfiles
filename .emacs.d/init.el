101177ec64718f42276cbb35154a07f46ce7bb86
Jordan Galby <gjordan90@gmail.com>
2014-11-29 09:32:12 +0100

    emacs: remove marmelade

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,7 +52,8 @@
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+                                        ;("marmalade" . "http://marmalade-repo.org/packages/")
+                         ))
 
 ;;(eval-when-compile (package-initialize))
 (package-initialize)
