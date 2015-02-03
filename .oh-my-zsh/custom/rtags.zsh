3d5e2452f4a777ea5f3f231db1a819473b714630
Jo <gjordan90@gmail.com>
2015-02-03 08:44:48 +0100

    rtags: update bin paths

--- .oh-my-zsh/custom/rtags.zsh
+++ .oh-my-zsh/custom/rtags.zsh
@@ -1,15 +1,19 @@
 #!/bin/zsh
 
-export RTAGS_PATH=$HOME/bin/rtags
+export RTAGS_BIN_PATH=$HOME/bin/rtags_bin
 export RTAGS_OLD_PATH=$PATH
 
 function rtagsoff() {
 	export PATH=$RTAGS_OLD_PATH
+    which gcc
+    which g++
 	rehash
 }
 
 function rtagson() {
 	export RTAGS_OLD_PATH=$PATH
-	export PATH=$RTAGS_PATH/bin:$PATH
+	export PATH=$RTAGS_BIN_PATH:$PATH
+    which gcc
+    which g++
 	rehash
 }
