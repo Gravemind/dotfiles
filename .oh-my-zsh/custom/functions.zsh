8d3b9b08fd39194f32a8fcabf3c1bf4e52e25323
Jordan Galby <gjordan90@gmail.com>
2015-05-16 17:38:13 +0200

    omz: use dtrx extractor everywhere

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -1,27 +1,15 @@
 #!/bin/zsh
 
 function wineprefix() {
-    export WINEARCH=win32
-    export WINEPREFIX=~/wineprefixes/win32
+	export WINEARCH=win32
+	export WINEPREFIX=~/wineprefixes/win32
 }
 
 function sx() {
-    cd $HOME
-    startx &
-    disown
-    exit
-}
-
-function xcd() {
-	FILEPATH=$(readlink -e "$1")
-	FILENAME="${FILEPATH##*/}"
-	DIRNAME="${FILENAME%.*}"
-	~/bin/xd $FILEPATH
-	if [[ $? -eq 0 ]]
-	then
-		echo "\ncd $DIRNAME\n"
-		cd $DIRNAME
-	fi
+	cd $HOME
+	startx &
+	disown
+	exit
 }
 
 function dolast() {
