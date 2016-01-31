be1aed0e320b03aa6d8fee7962f8bc32305bf248
Jordan <gravemind2a@gmail.com>
2016-01-31 19:46:54 +0100

    emacs: bump helm conf + helm-dash

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -802,12 +802,18 @@ of FILE in the current directory, suitable for creation"
     ;; must set before helm-config,  otherwise helm use default
     ;; prefix "C-x c", which is inconvenient because you can
     ;; accidentially pressed "C-x C-c"
-    (setq helm-command-prefix-key "C-x c")
+    ;(setq helm-command-prefix-key "C-x c")
 
     (require 'helm-config)
     (require 'helm-eshell)
     (require 'helm-files)
     (require 'helm-grep)
+
+    ;; Golden-ratio compat
+    (defun pl/helm-alive-p ()
+      (if (boundp 'helm-alive-p)
+          (symbol-value 'helm-alive-p)))
+    (add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p)
 
     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
     (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
@@ -817,28 +823,38 @@ of FILE in the current directory, suitable for creation"
     (define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
     (define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
 
+    (helm-autoresize-mode t)
+
     (setq
      helm-google-suggest-use-curl-p t
-     helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
+     helm-scroll-amount 8 ; scroll 4 lines other window using M-<next>/M-<prior>
      helm-quick-update t ; do not display invisible candidates
      helm-idle-delay 0.01 ; be idle for this many seconds, before updating in delayed sources.
      helm-input-idle-delay 0.01 ; be idle for this many seconds, before updating candidate buffer
+
+     helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
+     helm-split-window-default-side 'above ;; open helm buffer in another window
+
+     helm-autoresize-min-height 10
+     helm-autoresize-max-height 70
+
      helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
 
-     helm-split-window-default-side 'other ;; open helm buffer in another window
-     helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
-     helm-buffers-favorite-modes (append helm-buffers-favorite-modes
-                                         '(picture-mode artist-mode))
+     ;;  helm-split-window-default-side 'other ;; open helm buffer in another window
+     ;;  helm-buffers-favorite-modes (append helm-buffers-favorite-modes
+     ;;                                      '(picture-mode artist-mode))
      helm-candidate-number-limit 200 ; limit the number of displayed canidates
      helm-M-x-requires-pattern 0   ; show all candidates when set to 0
      helm-boring-file-regexp-list
      '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$") ; do not show these files in helm buffer
      helm-ff-file-name-history-use-recentf t
-     helm-move-to-line-cycle-in-source t ; move to end or beginning of source
-                                        ; when reaching top or bottom of source.
+
      ido-use-virtual-buffers t    ; Needed in helm-buffers-list
      helm-buffers-fuzzy-matching t      ; fuzzy matching buffer names when non--nil
                                         ; useful in helm-mini that lists buffers
+
+     ;; helm-move-to-line-cycle-in-source t ; move to end or beginning of source
+     ;;                                     ; when reaching top or bottom of source.
      )
 
     ;; Save current position to mark ring when jumping to a different place
@@ -846,7 +862,8 @@ of FILE in the current directory, suitable for creation"
 
     ;;(helm-mode 1)
 
-    ))
+    )
+    )
 
 ;;
 ;; ag
@@ -1016,6 +1033,22 @@ of FILE in the current directory, suitable for creation"
   :config
   (progn
     ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; disaster
+;;
+
+(req-package disaster
+  :bind ("C-c d" . disaster))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; helm dash (dash documentation sets)
+;;
+
+(req-package helm-dash)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
