d03865883e334a5df4875a55e6045dd370cb834d
Jordan <gravemind2a@gmail.com>
2016-02-05 16:07:47 +0100

    omz: update gcc/rtags

--- .oh-my-zsh/custom/gcc.zsh
+++ .oh-my-zsh/custom/gcc.zsh
@@ -1,21 +1,23 @@
 #!/bin/zsh
 
-function printcc() {
-	echo CC CXX $(which $CC $CXX)
+function whichCC() {
+	echo gcc g++ $(g++ --version | sed -nr 's/[^0-9.]*([0-9]+(\.[0-9]+)+).*/\1/p') $(which gcc g++)
+	echo clang clang++ $(clang++ --version | sed -nr 's/[^0-9.]*([0-9]+(\.[0-9]+)+).*/\1/p') $(which clang clang++)
+	echo CC CXX $CC $CXX
 }
 
 function gcc48() {
 	export CC=gcc-4.8
 	export CXX=g++-4.8
 	export CPP=$CXX
-	printcc
+	whichCC
 }
 
 function gcc49() {
 	export CC=gcc-4.9
 	export CXX=g++-4.9
 	export CPP=$CXX
-	printcc
+	whichCC
 }
 
 function gcclast() {
@@ -25,14 +27,33 @@ function gcclast() {
 	unset CC
 	unset CXX
 	unset CPP
-	printcc
+	whichCC
 }
 
 function clanglast() {
 	export CC=clang
 	export CXX=clang++
 	export CPP=$CXX
-	printcc
+	whichCC
 }
 
-gcclast
+export RTAGS_BIN_PATH=$HOME/bin/rtags_bin
+export RTAGS_OLD_PATH=$PATH
+
+function rtagsoff() {
+	export PATH=$RTAGS_OLD_PATH
+	rehash
+	whichCC
+}
+
+function rtagson() {
+	export RTAGS_OLD_PATH=$PATH
+	export PATH=$RTAGS_BIN_PATH:$PATH
+	rehash
+	whichCC
+}
+
+#gcclast
+clanglast
+
+# rtagson
