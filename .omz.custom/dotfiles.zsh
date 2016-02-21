f50608673786a0def52485e8ce57ac7fa03b6643
Gravemind <gravemind2a@gmail.com>
2016-02-21 22:33:34 +0100

    omz: dotfiles func toggle dotfiles git dir

--- .omz.custom/dotfiles.zsh
+++ .omz.custom/dotfiles.zsh
@@ -1,23 +1,6 @@
 #!/bin/zsh
 
-alias cdr='cd "$(git rev-parse --show-toplevel)"'
-
-# overwrite git plugin
-alias gdc=gdc
-
-function gitsetuser() {
-	local name="$1"
-	local email="$2"
-	[[ -n "$name" && -n "$email" ]] || { echo "INVALID name email"; return 1 }
-
-	git config --local user.name "$1"
-	git config --local user.email "$2"
-
-	git config user.name
-	git config user.email
-}
-
-dotfiles() {
+function dotfiles() {
 
 	ROOT="$HOME"
 	BARE="$HOME/.dotfiles.git"
