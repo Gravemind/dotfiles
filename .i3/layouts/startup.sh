1f4c96ba1378e199ed0b55e4aeb3cb7330b97d7e
Gravemind <gravemind2a@gmail.com>
2017-03-31 22:47:19 +0200

    laptop

--- .i3/layouts/startup.sh
+++ .i3/layouts/startup.sh
@@ -4,8 +4,8 @@ here="${0%/*}"
 
 $here/launch.sh -w 1 -- urxvt &
 disown
-$here/launch.sh -w 10 -- firefox &
-disown
+#$here/launch.sh -w 10 -- firefox &
+#disown
 
 i3-msg workspace 1
-i3-msg workspace 10
+#i3-msg workspace 10
