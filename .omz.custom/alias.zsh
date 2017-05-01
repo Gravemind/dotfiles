6ef607eca3178a39fdfb9baf720bec1391d75b4b
Gravemind <gravemind2a@gmail.com>
2017-05-01 14:08:24 +0200

    omz: better run-help (Alt-H)

--- .omz.custom/alias.zsh
+++ .omz.custom/alias.zsh
@@ -22,8 +22,8 @@ alias cleandhcp='sudo rm -rf /var/lib/dhcp3/*.lease'
 alias cleanhg='find . \( -name "*.orig" \) -print -delete'
 
 # trailing whitespace will make it resolve aliases !
-alias _='command sudo -E '
-alias sudo='command sudo -E '
+alias _='sudo -E '
+alias sudo='sudo -E '
 
 alias lock='i3lock -c 111111'
 alias disk='df -h --total'
---
+++ .omz.custom/help.zsh
@@ -0,0 +1,5 @@
+
+unalias run-help
+autoload -U run-help
+autoload run-help-git
+autoload run-help-sudo
