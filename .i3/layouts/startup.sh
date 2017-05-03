d8b742635a52fa3b1f5784c546778fd8a05b3bb7
Gravemind <gravemind2a@gmail.com>
2017-05-03 23:04:51 +0200

    i3: fix startup layout

--- .i3/layouts/startup.sh
+++ .i3/layouts/startup.sh
@@ -2,8 +2,10 @@
 
 here="${0%/*}"
 
-$here/launch.sh -w 1 -- urxvt
-$here/launch.sh -w 10 -- firefox
+$here/launch.sh -w 1 -- urxvt &
+disown
+$here/launch.sh -w 10 -- firefox &
+disown
 
 i3-msg workspace 1
 i3-msg workspace 10
