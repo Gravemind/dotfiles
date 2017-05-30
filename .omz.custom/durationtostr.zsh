597501773b109cd52933a9fdc4362bb50ffa8e97
Gravemind <gravemind2a@gmail.com>
2017-05-30 21:18:17 +0200

    omz: update durationtostr

--- .omz.custom/durationtostr.zsh
+++ .omz.custom/durationtostr.zsh
@@ -1,25 +1,23 @@
 #!/bin/zsh
 
-_subdurstr() {
-  STR=""
-  (( $1 > 0 )) && STR="${STR}$1 $2"
-  (( $1 > 1 )) && STR="${STR}s"
-  (( $1 > 0 )) && STR="${STR} "
-  echo "$STR"
-}
-
 durationtostr() {
-  local T=$1
-  local D=$((T/60/60/24))
-  local H=$((T/60/60%24))
-  local M=$((T/60%60))
-  local S=$((T%60))
-  STR="$(_subdurstr $D day)$(_subdurstr $H hour)"
-  if [[ -n "$STR" ]]
-  then
-	  echo "$STR$2"
-  else
-	  STR="$(_subdurstr $M min)$(_subdurstr $S second)"
-	  echo "$STR$2"
-  fi
+  local t=$1
+  local years=$(($t/31556736))
+  t=$(($t - $years * 31556736))
+  local months=$(($t/2630016))
+  t=$(($t - $months * 2630016))
+  local days=$(($t/86400))
+  t=$(($t - $days * 86400))
+  local hours=$(($t/3600))
+  t=$(($t - $hours * 3600))
+  local mins=$(($t/60))
+  t=$(($t - $mins * 60))
+  local secs=$t
+  [[ "$1" -ge 31556736 && true ]] && echo -n "${years}y "
+  [[ "$1" -ge 2630016 && "$1" -lt $((31556736 * 5)) ]] && echo -n "${months}m "
+  [[ "$1" -ge 86400 && "$1" -lt $((2630016 * 5))  ]] && echo -n "${days}d "
+  [[ "$1" -ge 3600 && "$1" -lt $((86400 * 5)) ]] && echo -n "${hours}h "
+  [[ "$1" -ge 60 && "$1" -lt $((3600 * 5)) ]] && echo -n "${years}m "
+  [[ "$1" -ge 0 && "$1" -lt $((60 * 5)) ]] && echo -n "${years}s "
+  echo "$2"
 }
