6e6591218ffe3896d4dd07f45ddebd3cd23a77ac
Gravemind <gravemind2a@gmail.com>
2017-07-19 09:41:03 +0200

    emacs tramp sudo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -261,9 +261,24 @@
 ;; ido
 ;;
 
+;; https://www.emacswiki.org/emacs/TrampMode#toc20
+(defun find-alternative-file-with-sudo ()
+  (interactive)
+  (let ((fname (or buffer-file-name
+                   dired-directory)))
+    (when fname
+      (if (string-match "^/sudo:root@localhost:" fname)
+          (setq fname (replace-regexp-in-string
+                       "^/sudo:root@localhost:" ""
+                       fname))
+        (setq fname (concat "/sudo:root@localhost:" fname)))
+      (find-alternate-file fname))))
+
 (req-package ido
   :bind (("C-x C-f" . ido-find-file)
-         ("C-x b"   . ido-switch-buffer))
+         ("C-x b"   . ido-switch-buffer)
+         ("C-x C-r" . find-alternative-file-with-sudo)
+         )
   :config
     (setq-default
      ido-enable-flex-matching t
@@ -275,6 +290,7 @@
      ;; ido-use-filename-at-point 'guess
      )
     (ido-mode 1)
+
     )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
