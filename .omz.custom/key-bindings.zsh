7d15cdcd95bfa70f1952f261df979b808044531c
Gravemind <gravemind2a@gmail.com>
2017-07-23 23:11:36 +0200

    omz: ctrl key bindings

---
+++ .omz.custom/key-bindings.zsh
@@ -0,0 +1,8 @@
+
+bindkey "\eOc" emacs-forward-word       # [Ctrl-Right]
+bindkey "\eOd" emacs-backward-word      # [Ctrl-Left]
+bindkey "^H" backward-delete-word       # [Ctrl-Backspace]
+bindkey "\e[3^" delete-word             # [Ctrl-Del]
+
+bindkey "\e[8^" end-of-line             # [Ctrl-End]
+bindkey "\e[7^" beginning-of-line       # [Ctrl-Home]
