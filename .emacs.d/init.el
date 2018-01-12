2708fbfd8a9bc90e34077a6159da0bd701e80b84
Gravemind <gravemind2a@gmail.com>
2018-01-12 18:22:08 +0100

    emacs magit: re-enable log switch ignore space at eol

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -872,9 +872,10 @@ With argument, do this that many times."
     'magit-pull-popup
     ?a "Auto stash" "--autostash")
 
-  ;(magit-define-popup-switch
-  ;  'magit-diff-popup
-  ;  ?W "Ignore changes in whitespace at EOL" "--ignore-space-at-eol")
+  (magit-define-popup-switch
+    'magit-diff-popup
+    ?W "Ignore changes in whitespace at EOL" "--ignore-space-at-eol")
+
   (magit-define-popup-switch
     'magit-diff-popup
     ?b "Ignore changes in amount of whitespace" "--ignore-space-change")
