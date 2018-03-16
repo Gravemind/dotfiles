dc0a7ada3ea40abd358a1a70b7b65b15ae39c9ab
Gravemind <gravemind2a@gmail.com>
2018-03-16 12:44:48 +0100

    omz: add history-substring-search config
    case sensitive + remove duplicates

---
+++ .omz.custom/history-substring-search.zsh
@@ -0,0 +1,12 @@
+
+# skip duplicate _adjacent_ search results
+#setopt HIST_FIND_NO_DUPS
+# skip all duplicates
+setopt HIST_IGNORE_ALL_DUPS
+
+# case sensitive
+HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS=I
+# case insensitive
+#HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS=i
+# lower match lower and upper, upper match upper only
+#HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS=l
