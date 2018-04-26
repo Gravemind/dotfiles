6a96206154ef798ad119cce8e067103c2103a493
Gravemind <gravemind2a@gmail.com>
2018-04-26 16:03:27 +0200

    zsh: more `rm *` protection (wait 10s)

--- .omz.custom/zsh.zsh
+++ .omz.custom/zsh.zsh
@@ -1,9 +1,14 @@
 #!/bin/zsh
 
+## http://zsh.sourceforge.net/Doc/Release/Options.html
+
 unsetopt share_history
-unsetopt SHARE_HISTORY
 unsetopt correctall
 #unsetopt extended_glob
 
 ## unset option that nices background jobs
-unsetopt BG_NICE
+unsetopt bg_nice
+
+## wait 10 seconds for 'rm *'.
+## (force stop wait with Ctrl-C, or expand the '*' with TAB)
+setopt rm_star_wait
