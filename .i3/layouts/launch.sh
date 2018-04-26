55e0ea091e32cb4b244a5de8c4fa77fdb0fbe8b7
Gravemind <gravemind2a@gmail.com>
2018-04-26 16:07:30 +0200

    i3 launch placeholder: add quiet, improve

--- .i3/layouts/launch.sh
+++ .i3/layouts/launch.sh
@@ -8,23 +8,32 @@ instance=
 usage() {
 	echo "usage: $0 [-w WORKSPACE] [-c WIN_CLASS] [-i WIN_INSTANCE] [--] command [args...]"
 	echo "	Spawns a i3 placeholder for command \"command\""
-	echo "	  -w WORKSPACE		: move placeholder to workspace WORKSPACE (default: current)"
-	echo "	  -c WIN_CLASS		: placeholder window's \"class\" to match (default: command)"
-	echo "	  -i WIN_INSTANCE	: placeholder window's \"instance\" to match (no default)"
+	echo "	  -w WORKSPACE    : move placeholder to workspace WORKSPACE (default: current)"
+	echo "	  -c WIN_CLASS    : placeholder window's \"class\" to match (default: command)"
+	echo "	  -i WIN_INSTANCE : placeholder window's \"instance\" to match (no default)"
+	echo "	  -q              : be quiet about it"
 }
-options=`getopt -o "+hfw:c:i:" -n "$0" -- "$@"`
+options=`getopt -o "+hqw:c:i:" -n "$0" -- "$@"`
+[[ $? -eq 0 ]] || exit 1
 eval set -- "$options"
+quiet=0
 while true; do
 	case "$1" in
 		-h) usage; exit 0 ;;
 		-w) workspace="$2"; shift ;;
 		-c) class="$2"; shift ;;
 		-i) instance="$2"; shift ;;
+		-q) quiet=1; ;;
 		--) shift; break ;;
 		*) break ;; ## no flags after first non-flag arg
 	esac
 	shift
 done
+
+log() {
+	[[ $quiet -eq 0 ]] || return;
+	echo "$0:" "$@"
+}
 
 if [ "$#" -lt 1 ]; then
 	echo "$0: not enough arguments"
@@ -47,7 +56,7 @@ else
 	instance_line=
 fi
 
-tmp=`mktemp`
+tmp="$(mktemp -t "i3layoutlaunch.XXXXXXXX")"
 trap "{ rm -f $tmp; }" EXIT
 
 layout_win_name="[[ ${name} ]]"
@@ -62,7 +71,7 @@ sponge $tmp <<EOF
 }
 EOF
 
-echo "$0: launching \"$prog\" class \"$class\" instance \"$instance\""
+log "launching \"$prog\" class \"$class\" instance \"$instance\""
 
 $here/append_layout_window.sh $tmp $workspace > /dev/null
 
@@ -76,13 +85,13 @@ then
 	winid="$(xwininfo -name "$layout_win_name" -int 2> /dev/null | sed -nE 's/.*Window id: ([0-9]+) .*/\1/gp')"
 	if [[ -n "$winid" ]]
 	then
-		echo "$0: $prog exited $res (closing placeholder $winid)"
+		log "$prog exited $res (closing placeholder $winid)"
 		xkill -id "$winid" > /dev/null
 	else
-		echo "$0: $prog exited $res (no more placeholder)"
+		log "$prog exited $res (no more placeholder)"
 	fi
 else
-	echo "$0: $prog exited $res"
+	log "$prog exited $res"
 fi
 
 exit $res
