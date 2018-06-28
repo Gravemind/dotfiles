b1ad3d44d4beeaa3f51da7a2bb61d59d15455050
Gravemind <gravemind2a@gmail.com>
2018-06-28 10:35:53 +0200

    omz my_laptop update

--- .omz.custom/plugins/my_laptop/my_laptop.plugin.zsh
+++ .omz.custom/plugins/my_laptop/my_laptop.plugin.zsh
@@ -5,8 +5,24 @@ alias wscan='iwlist wlan0 scanning'
 
 nvhdmionly() {
 	nvidia-settings --assign 'CurrentMetaMode=HDMI-0: nvidia-auto-select +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}'
+	sleep 0.2
 	## must set dpi with LVDS-1-1 enabled or screen freeze (nv bug?)
 	xrandr --output LVDS-1-1 --auto --output HDMI-0 --auto --dpi 94
+	sleep 0.2
 	xset dpms force off
+	sleep 0.2
 	xrandr --output LVDS-1-1 --off
+	sleep 0.2
+}
+
+nvhdmionlyunsync() {
+	nvidia-settings --assign 'CurrentMetaMode=HDMI-0: nvidia-auto-select +0+0 {ForceCompositionPipeline=Off, ForceFullCompositionPipeline=Off}'
+	sleep 0.2
+	## must set dpi with LVDS-1-1 enabled or screen freeze (nv bug?)
+	xrandr --output LVDS-1-1 --auto --output HDMI-0 --auto --dpi 94
+	sleep 0.2
+	xset dpms force off
+	sleep 0.2
+	xrandr --output LVDS-1-1 --off
+	sleep 0.2
 }
