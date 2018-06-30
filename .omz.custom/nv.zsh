4d63a722eca4e71da50829412fd08cfd02bf3963
Gravemind <gravemind2a@gmail.com>
2018-06-30 13:36:11 +0200

    omz nv commands update

--- .omz.custom/nv.zsh
+++ .omz.custom/nv.zsh
@@ -1,16 +1,40 @@
 
 nvdual() {
-    nvidia-settings --assign "CurrentMetaMode=DPY-1: nvidia-auto-select @1680x1050 +0+0 {ViewPortIn=1680x1050, ViewPortOut=1680x1050+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DPY-6: nvidia-auto-select @1920x1200 +1680+0 {ViewPortIn=1920x1200, ViewPortOut=1920x1200+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+	nvidia-settings --assign 'CurrentMetaMode=DP-4: nvidia-auto-select +1680+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, HDMI-0: nvidia-auto-select +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}'
 }
 
 nvdualunsync() {
-    nvidia-settings --assign "CurrentMetaMode=DPY-1: nvidia-auto-select @1680x1050 +0+0 {ViewPortIn=1680x1050, ViewPortOut=1680x1050+0+0, ForceCompositionPipeline=Off, ForceFullCompositionPipeline=Off}, DPY-6: nvidia-auto-select @1920x1200 +1680+0 {ViewPortIn=1920x1200, ViewPortOut=1920x1200+0+0, ForceCompositionPipeline=Off, ForceFullCompositionPipeline=Off}"
+	nvidia-settings --assign 'CurrentMetaMode=DP-4: nvidia-auto-select +1680+0 {ForceCompositionPipeline=Off, ForceFullCompositionPipeline=Off}, HDMI-0: nvidia-auto-select +0+0 {ForceCompositionPipeline=Off, ForceFullCompositionPipeline=Off}'
 }
 
 nvsingle() {
-    nvidia-settings --assign "CurrentMetaMode=DPY-6: nvidia-auto-select @1920x1200 +0+0 {ViewPortIn=1920x1200, ViewPortOut=1920x1200+0+0}"
+	nvidia-settings --assign 'CurrentMetaMode=DP-4: nvidia-auto-select +1680+0 {ForceCompositionPipeline=Off, ForceFullCompositionPipeline=Off}'
 }
 
 nvsinglesync() {
-    nvidia-settings --assign "CurrentMetaMode=DPY-6: nvidia-auto-select @1920x1200 +0+0 {ViewPortIn=1920x1200, ViewPortOut=1920x1200+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+	nvidia-settings --assign 'CurrentMetaMode=DP-4: nvidia-auto-select +1680+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}'
+}
+
+nvoverclock() {
+	# Power limit
+	# query Max Power Limit with: nvidia-smi -q | grep Power
+	sudo nvidia-smi -pl 216
+
+	# clock offsets
+	nvidia-settings -a "[gpu:0]/GPUMemoryTransferRateOffset[3]=400"
+	nvidia-settings -a "[gpu:0]/GPUGraphicsClockOffset[3]=200"
+
+	# GPUPowerMizerMode
+	# - 0 adaptive
+	# - 1 high perf
+	# - 2 auto
+	nvidia-settings -a "GPUPowerMizerMode=1"
+}
+
+
+nvoverclockoff() {
+	sudo nvidia-smi -pl 200
+	nvidia-settings -a "[gpu:0]/GPUMemoryTransferRateOffset[3]=0"
+	nvidia-settings -a "[gpu:0]/GPUGraphicsClockOffset[3]=0"
+	nvidia-settings -a "GPUPowerMizerMode=2"
 }
