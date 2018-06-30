ed03777025b5ec4f67c15ca3991221ed60c9d2e6
Gravemind <gravemind2a@gmail.com>
2018-06-30 13:37:35 +0200

    omz: more dotfiles utils

--- .omz.custom/dotfiles.zsh
+++ .omz.custom/dotfiles.zsh
@@ -1,18 +1,25 @@
 #!/bin/zsh
 
+#
+# bare git dotfiles:
+#
+# https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
+# https://news.ycombinator.com/item?id=11071754
+#
+
+DOTFILES_ROOT="$HOME"
+DOTFILES_BARE="$HOME/.dotfiles.git"
+
 # toggles env GIT_DIR and GIT_WORK_TREE to home dotfiles git
 # makes git dotfiles works in magit
 function dotfiles() {
-	ROOT="$HOME"
-	BARE="$HOME/.dotfiles.git"
-
 	if [[ -z "$GIT_DIR" && -z "$GIT_WORK_TREE" ]]
 	then
-		export GIT_DIR="$BARE"
-		export GIT_WORK_TREE="$ROOT"
+		export GIT_DIR="$DOTFILES_BARE"
+		export GIT_WORK_TREE="$DOTFILES_ROOT"
 		echo "Git is now forced to $GIT_WORK_TREE ($GIT_DIR)"
 	else
-		if [[ "$GIT_DIR" != "$BARE" && "$GIT_WORK_TREE" != "$BARE" ]]
+		if [[ "$GIT_DIR" != "$DOTFILES_BARE" && "$GIT_WORK_TREE" != "$DOTFILES_BARE" ]]
 		then
 			echo unknown current "$GIT_DIR" "$GIT_WORK_TREE"
 			echo will do nothing
@@ -22,4 +29,25 @@ function dotfiles() {
 		unset GIT_WORK_TREE
 		echo "Git is back to normal"
 	fi
+}
+
+# alias git
+alias dot="git --git-dir=$DOTFILES_BARE --work-tree=$DOTFILES_ROOT"
+
+# dotfiles command (dotc emacs)
+function dotc() {
+	(
+		export GIT_DIR="$DOTFILES_BARE"
+		export GIT_WORK_TREE="$DOTFILES_ROOT"
+		"$@"
+	)
+}
+
+# dotfiles status
+function dots() {
+	(
+		export GIT_DIR="$DOTFILES_BARE"
+		export GIT_WORK_TREE="$DOTFILES_ROOT"
+		git ls-files | sed -E 's#(.*)/.*?#\1#g' | sort -u | xargs git s
+	)
 }
