fd5d994e8beb2c30edd7d76014f68e7de422a624
Gravemind <gravemind2a@gmail.com>
2018-08-09 15:01:09 +0200

    omz pacman: replace pacaur by yay

--- .omz.custom/plugins/my_pacman/my_pacman.plugin.zsh
+++ .omz.custom/plugins/my_pacman/my_pacman.plugin.zsh
@@ -1,15 +1,27 @@
 #!/bin/zsh
 
-alias pacman='env -u CC -u CXX -u CPP pacman '
-alias pacaur='env -u CC -u CXX -u CPP pacaur '
-alias pac='env -u CC -u CXX -u CPP pacaur '
-
 export PACPEND='-g explicit -s name,vdiff'
 
-if ! command which pacaur > /dev/null
+if ! command which yay > /dev/null
 then
-	echo "${fg_bold[red]}pacaur not found${reset_color}"
+	echo "${fg_bold[red]}yay not found${reset_color}"
 fi
+
+pacman() env -u CC -u CXX -u CPP $(which -p pacman) "$@"
+pacaur() env -u CC -u CXX -u CPP $(which -p pacaur) "$@"
+yay()    env -u CC -u CXX -u CPP $(which -p yay) "$@"
+
+alias pac='yay'
+
+# Makes run-help (Alt-H) on yay run `man yay` then `man pacman`
+run-help-yay() {
+	man yay
+	builtin print -nP "%SPress any key for more help or q to quit%s"
+	builtin read -k what
+	[[ $what != $newline ]] && echo
+	[[ $what == [qQ] ]] && return
+	man pacman
+}
 
 # Update mirror list if out dated
 pacupmir() {
@@ -58,18 +70,20 @@ pacup() {
 		return 1
 	fi
 
-	# same as default of checkupdates
+	# same as checkupdate's default
 	export CHECKUPDATES_DB="${TMPDIR:-/tmp}/checkup-db-${USER}/"
 
 	if [[ ! -e "$CHECKUPDATES_DB" ]]
 	then
+		# (uses CHECKUPDATES_DB)
 		checkupdates > /dev/null
 	fi
 
-	## `pacaur` does not catch pacman errors and continues with AUR packages silently, so run pacman alone
+	## (`pacaur` does not catch pacman errors and continues with AUR packages silently, so run pacman alone)
 	( fakeroot -- pacman -Syuw --noconfirm --dbpath "$CHECKUPDATES_DB" --color=always || { echo "${fg_bold[red]}$0: pacman -Syuw failed !!$reset_color"; return 1; } )
 	# ( pacaur --aur -Suw --noconfirm --noedit--color=always || { echo "${fg_bold[red]}$0: pacaur --aur -Syuw failed !!$reset_color"; return 1; } ) | grep -v '^$'
 
+	# (uses CHECKUPDATES_DB)
 	~/bin/pacpend --aur
 
 	if [[ $? -eq 0 ]]
@@ -85,13 +99,16 @@ pacupg() {
 
 	pacupmir || { echo "$0: pacupmir failed"; return 1; }
 
-	pacaur -Sy > /dev/null
-	~/bin/pacpend -b ''
+	yay -Sy > /dev/null
+
+	~/bin/pacpend -b '' --aur
 
 	## `pacaur` does not catch pacman errors and continues with AUR packages silently, so run pacman alone
 	## and `pacaur -Syur` exits 1 ?
-	sudo pacman -Syu --noconfirm "$@" || { echo "${fg_bold[red]}$0: pacman -Syu failed !!$reset_color"; return 1; }
-	pacaur --aur -Syu --noconfirm --noedit "$@" || { echo "${fg_bold[red]}$0: pacaur --aur -Syu failed !!$reset_color"; return 1; }
+	#sudo pacman -Syu --noconfirm "$@" || { echo "${fg_bold[red]}$0: pacman -Syu failed !!$reset_color"; return 1; }
+	#pacaur --aur -Syu --noconfirm --noedit "$@" || { echo "${fg_bold[red]}$0: pacaur --aur -Syu failed !!$reset_color"; return 1; }
+
+	yay -Syu --noconfirm "$@" || { echo "${fg_bold[red]}$0: yay -Syu failed !!$reset_color"; return 1; }
 
 	echo "${fg_bold[green]}packages updated$reset_color"
 
@@ -127,7 +144,7 @@ pacfind() {
 	fi
 	if [[ $age -lt 0 || $age -gt $maxage ]]
 	then
-		echo "$0: pacman files db is ${fg_bold[red]}$(durationtostr $age old)${reset_color} ! running pac -Fy:"
+		echo "$0: pacman files db is ${fg_bold[red]}$(durationtostr $age old)${reset_color} ! updating: pac -Fy:"
 		pac -Fy
 	else
 		echo "$0: pacman files db is $(durationtostr $age old) (updating at $(durationtostr $maxage old))"
@@ -150,17 +167,25 @@ pkgpacfind() {
 	fi
 
 	local cachefile="/var/cache/pkgfile/core.files"
-	local age=$(( $(date +%s) - $(date +%s -r "$cachefile") ))
-	local maxage=$((3600 * 24 * 30)) ## 30 days
-	if [[ $age -gt $maxage ]]
+	if [[ ! -e "$cachefile" ]]
 	then
-		echo "$0: pkgfile cache is ${fg_bold[red]}$(durationtostr $age old)${reset_color} ! running sudo pkgfile -u:"
+		echo "$0: no pkgfile cache ! running sudo pkgfile -u:"
 		sudo pkgfile -u
 	else
-		echo "$0: pkgfile cache is $(durationtostr $age old) (updating at $(durationtostr $maxage old))"
+		local age=$(( $(date +%s) - $(date +%s -r "$cachefile") ))
+		local maxage=$(( 3600 * 24 * 30 )) ## 30 days
+		if [[ $age -gt $maxage ]]
+		then
+			echo "$0: pkgfile cache is ${fg_bold[red]}$(durationtostr $age old)${reset_color} ! running sudo pkgfile -u:"
+			sudo pkgfile -u
+		else
+			echo "$0: pkgfile cache is $(durationtostr $age old) (updating at $(durationtostr $maxage old))"
+		fi
 	fi
 
-	pkgs=(${(f)"$(pkgfile -b -v -- "$cmd" 2>/dev/null)"})
+	#args="-b" # bin sbin only
+
+	pkgs=(${(f)"$(pkgfile -v $args -- "$cmd" 2>/dev/null)"})
 	if [[ -n "$pkgs" ]]; then
 		printf '%s may be found in the following packages:\n' "$cmd"
 		printf '  %s\n' $pkgs[@]
