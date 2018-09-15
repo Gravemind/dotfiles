12aa1dadd778509ab679f770453c5c8f2d82a578
Gravemind <gravemind2a@gmail.com>
2018-09-15 22:53:13 +0200

    omz: more emacs key bindings

--- .omz.custom/key-bindings.zsh
+++ .omz.custom/key-bindings.zsh
@@ -2,9 +2,28 @@
 # tested on urxvt
 # also see ~/.inputrc
 
+# emacs
 bindkey "\eOc" emacs-forward-word       # [Ctrl-Right]
 bindkey "\eOd" emacs-backward-word      # [Ctrl-Left]
-bindkey "^H" backward-delete-word       # [Ctrl-Backspace]
 bindkey "\e[3^" delete-word             # [Ctrl-Del]
 bindkey "\e[8^" end-of-line             # [Ctrl-End]
 bindkey "\e[7^" beginning-of-line       # [Ctrl-Home]
+bindkey '^W' kill-region				# [Ctrl-W]
+bindkey '^G' deactivate-region			# [Ctrl-G]
+bindkey '^I' complete-word				# [Ctrl-I]
+
+copy-and-deactivate-region() {
+	zle copy-region-as-kill
+	zle deactivate-region
+}
+zle -N copy-and-deactivate-region
+bindkey '^[w' copy-and-deactivate-region # [Meta-W]
+
+if [[ "$TERM" = rxvt* ]]
+then
+	bindkey "^H" backward-delete-word   # [Ctrl-Backspace]
+fi
+
+# other
+bindkey "^[\'" quote-region				# [Ctrl-']
+bindkey '^["' quote-line				# [Ctrl-"]
