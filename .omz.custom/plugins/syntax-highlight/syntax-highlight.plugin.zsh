151a5bb7e1d13e67a716859303562b082905f6d6
Gravemind <gravemind2a@gmail.com>
2018-09-26 11:51:36 +0200

    omz: bump submodules

--- .omz.custom/plugins/syntax-highlight/syntax-highlight.plugin.zsh
+++ .omz.custom/plugins/syntax-highlight/syntax-highlight.plugin.zsh
@@ -1,7 +1,8 @@
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(line main)
 
-source "${0:a:h}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+0=${(%):-%N}
+source ${0:A:h}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # add black background to all styles
 for k in "${(@k)ZSH_HIGHLIGHT_STYLES}"; do
