baf957e92ca7b19b2c4f250f18196c1749d7104d
Gravemind <gravemind2a@gmail.com>
2018-10-01 16:24:40 +0200

    i3: add flatpak apps to program launcher

---
+++ .i3/rofi_flatpak.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ -z "$*" ]]
+then
+    flatpak list --app | cut -d/ -f1
+else
+    app="$1"
+    logfile="/tmp/rofi_flatpak.$app.log"
+    date > "$logfile"
+    flatpak run "$app" >& "$logfile" &
+    disown
+fi
+
