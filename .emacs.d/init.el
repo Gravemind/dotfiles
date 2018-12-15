105d1938e156250b04f216f2912da15fbfe8245d
Jordan Galby <gravemind2a@gmail.com>
2018-12-15 20:00:13 +0100

    emacs: add sua hop

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -303,58 +303,79 @@
 ;; Tramp
 ;;
 
-(setq-default
- tramp-verbose 2 ; warnings
- ;tramp-verbose 3 ; + connections (default)
- ;tramp-verbose 6 ; verbose
- ;tramp-connection-timeout 10
- tramp-connection-timeout 14400 ; 4h
- password-cache-expiry nil
- ;tramp-chunksize 4050
+(req-package tramp
+  :pin manual
+  :defer t
 
- ;; Simpler prompt when `TERM=dump ssh ...`
- ;;   https://www.emacswiki.org/emacs/TrampMode#toc9
- ;;
- ;; bash: [[ "$TERM" == "dumb" ]] && { export PS1='$ '; return; }
- ;;   can be added to: ~/.bashrc, /etc/bash.bashrc, /etc/profile ...
- ;;
- ;; zsh: see ~/.zshrc
- ;;
- tramp-shell-prompt "\\$ "
+  :config
+  (setq-default
+   tramp-verbose 2 ; warnings
+   ;;tramp-verbose 3 ; + connections (default)
+   ;;tramp-verbose 6 ; verbose
+   ;;tramp-connection-timeout 10
+   tramp-connection-timeout 14400 ; 4h
+   password-cache-expiry nil
+   ;;tramp-chunksize 4050
 
- tramp-use-ssh-controlmaster-options t
- tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath=~/.ssh/tramp.%%C -o ControlPersist=no"
+   ;; Simpler prompt when `TERM=dump ssh ...`
+   ;;   https://www.emacswiki.org/emacs/TrampMode#toc9
+   ;;
+   ;; bash: [[ "$TERM" == "dumb" ]] && { export PS1='$ '; return; }
+   ;;   can be added to: ~/.bashrc, /etc/bash.bashrc, /etc/profile ...
+   ;;
+   ;; zsh: see ~/.zshrc
+   ;;
+   tramp-shell-prompt "\\$ "
+
+   tramp-use-ssh-controlmaster-options t
+   tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath=~/.ssh/tramp.%%C -o ControlPersist=no"
+   )
+
+  ;;
+  ;; Custom 'ssha' hop: '/ssha:user@addr:/home/user'
+  ;; - Enables ssh agent forwarding
+  ;; - Forces control master (the default disables it when multi-hop, even when first hop)
+  ;;
+  ;; See /usr/share/emacs/26.1/lisp/net/tramp-sh.el.gz
+  ;;
+  (add-to-list
+   'tramp-methods
+   '("ssha"
+     (tramp-login-program        "ssh")
+     (tramp-login-args           (("-l" "%u") ("-p" "%p") ; ("%c")
+
+                                  ;; Force control master
+                                  ("-o" "ControlMaster=auto" "-o" "ControlPath=~/.ssh/tramp.%%C" "-o" "ControlPersist=no")
+                                  ;; Forward agent
+                                  ("-A")
+
+                                  ("-e" "none") ("%h")))
+     (tramp-async-args           (("-q")))
+     (tramp-remote-shell         "/bin/sh")
+     (tramp-remote-shell-login   ("-l"))
+     (tramp-remote-shell-args    ("-c"))))
+
+  ;;
+  ;; Custom 'sua' hop
+  ;;   sudo with ssh agent forwarding
+  ;;   see ~/bin/sua
+  ;;
+  (add-to-list
+   'tramp-methods
+   '("sua"
+     (tramp-login-program        "~/bin/sua")
+     ;; The password template must be masked.  Otherwise, it could be
+     ;; interpreted as password prompt if the remote host echoes the command.
+     (tramp-login-args           (("%u")
+                                  ("-p" "P\"\"a\"\"s\"\"s\"\"w\"\"o\"\"r\"\"d\"\":")))
+     ;; Local $SHELL could be a nasty one, like zsh or fish.  Let's override it.
+     (tramp-login-env            (("SHELL") ("/bin/sh")))
+     (tramp-remote-shell         "/bin/sh")
+                                        ;(tramp-remote-shell-login   ("-l"))
+     (tramp-remote-shell-args    ("-c"))
+     (tramp-connection-timeout   10)))
+
 )
-
-;;
-;; Custom 'ssha' hop: '/ssha:user@addr:/home/user'
-;; - Enables ssh agent forwarding
-;; - Forces control master (the default disables it when multi-hop, even when first hop)
-;;
-(eval-after-load "tramp"
-  '(add-to-list 'tramp-methods
-                (list "ssha"
-                      '(tramp-login-program "ssh")
-                      '(tramp-login-args
-                        (("-l" "%u")
-                         ("-p" "%p")
-                         ;; Force control master
-                         ("-o" "ControlMaster=auto" "-o" "ControlPath=~/.ssh/tramp.%%C" "-o" "ControlPersist=no") ;("%c")
-                         ;; Forward agent
-                         ("-A")
-                         ("-e" "none")
-                         ("%h")
-                         ;;("-t" "bash")
-                         ))
-                      '(tramp-async-args
-                        (("-q")))
-                      '(tramp-remote-shell "/bin/sh")
-                      '(tramp-remote-shell-login
-                        ("-l"))
-                      '(tramp-remote-shell-args
-                        ("-c")))
-                t)
-  )
 
 ;; might help tramp
 ;;(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
