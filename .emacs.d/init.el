f56dd494d1c50931a36e5c138dd6e7e3304d82ab
Jordan Galby <gravemind2a@gmail.com>
2019-08-15 16:48:16 +0200

    emacs: reword indentation functions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -384,7 +384,7 @@
   (require 'dtrt-indent)
   (concat
    ;; ↹ → ⇀ ⇁ ↦ ⇥
-   (format "%s" (symbol-value (jo/dtrt-offset-var)))
+   (format "%s" (symbol-value (jo--indent-offset-var)))
    (if indent-tabs-mode "↹" "∙")
    (format "%s" tab-width)
    (if dtrt-indent-original-indent
@@ -935,18 +935,18 @@
   :config
   ;;(editorconfig-mode 1)
   (setq-default editorconfig-mode-lighter " EdConf")
-  (add-hook 'editorconfig-after-apply-functions
-            (lambda (props)
-              ;; set tab-stop-list
-              (when-let ((indent_size (gethash 'indent_size props)))
-                (when-let (((editorconfig-string-integer-p indent_size))
-                           (indent_size (string-to-number indent_size)))
-                  (setq tab-stop-list (number-sequence indent_size 200 indent_size))
-                  ))
-              ;;(message "editorconfig %s" props)
-              (whitespace-mode 0)
-              (whitespace-mode +1)
-              ))
+  ;; (add-hook 'editorconfig-after-apply-functions
+  ;;           (lambda (props)
+  ;;             ;; set tab-stop-list
+  ;;             (when-let ((indent_size (gethash 'indent_size props)))
+  ;;               (when-let (((editorconfig-string-integer-p indent_size))
+  ;;                          (indent_size (string-to-number indent_size)))
+  ;;                 (setq tab-stop-list (number-sequence indent_size 200 indent_size))
+  ;;                 ))
+  ;;             ;;(message "editorconfig %s" props)
+  ;;             (whitespace-mode 0)
+  ;;             (whitespace-mode +1)
+  ;;             ))
 )
 
 ;;
@@ -1002,92 +1002,115 @@
  )
 
 (setq-default whitespace-style '(face trailing indentation space-before-tab))
-(add-hook 'prog-mode-hook (lambda () (whitespace-mode 1)))
+;; (add-hook 'prog-mode-hook (lambda () (whitespace-mode 1)))
 
-(defun jo/dtrt-offset-var ()
+(defun jo--indent-offset-var ()
+  "Returns the symbol of the variable that defines indentation
+for the current major mode (uses dtrt)"
   (require 'dtrt-indent)
   (nth 2 (dtrt-indent--search-hook-mapping major-mode)))
 
-(defun jo/default-indentation ()
-  "Setup indentation"
-  (require 'editorconfig)
+(defun jo--on-indentation-changed ()
+  "Finish setuping indentation"
+  (let ((indent_size (symbol-value (jo--indent-offset-var))))
+    (when (not (eq (car tab-stop-list) indent_size))
+      ;;(message "set tab-stop-list")
+      (setq tab-stop-list (number-sequence indent_size 200 indent_size))
+      ))
+  (whitespace-mode 0)
+  ;; (setq whitespace-style
+  ;;       '(face trailing indentation space-before-tab
+  ;;         ))
+  (whitespace-mode 1)
+  )
+
+(defun jo--set-indent-vars (offset tabs tabw)
+  (setq indent-tabs-mode (if tabs t nil)
+        tab-width        tabw)
+  (let ((offset-var (jo--indent-offset-var)))
+    (set offset-var offset)
+    (message "setup-indentation %s: tabs:%s tabw:%s %s:%s" major-mode indent-tabs-mode tab-width offset-var (symbol-value offset-var))
+    )
+)
+
+(defun jo--my-default-indentations-apply ()
+  "Lookup my-default-indentations* and apply it"
+
   (require 'dtrt-indent)
   (when-let ((indentations
               (cond ((assoc major-mode my-default-indentations-by-mode))
                     ((assoc (nth 1 (dtrt-indent--search-hook-mapping major-mode)) my-default-indentations-by-dtrt-lang))))
              )
-    (message "indentations: %s" indentations)
-    (jo/setup-indentation (nth 1 indentations) (nth 2 indentations) (nth 3 indentations) nil)
+    ;;(message "indentations: %s" indentations)
+    (jo--set-indent-vars (nth 1 indentations) (nth 2 indentations) (nth 3 indentations))
     )
+  )
+(defun jo/my-default-indentations-apply ()
+  "Lookup my-default-indentations* and apply it"
+  (interactive)
+  (jo--my-default-indentations-apply)
+  (jo--on-indentation-changed)
+)
 
-  (dtrt-indent-adapt)
-  (editorconfig-mode-apply)
+(defun jo/force-indentation (offset tabs tabw)
+  "Forces a new indentation."
+  (interactive (list
+                (read-number (concat "Indent offset (" (symbol-name (jo--indent-offset-var)) ") ? ")
+                             (symbol-value (jo--indent-offset-var)))
+                (y-or-n-p (concat "Indent with tabs ? (default " (if indent-tabs-mode "y" "n")  ") "))
+                (read-number (concat "Tab width ? ") tab-width)
+                ))
 
-  ;; re-setup tab-stop-list if offset changed
-  ;; (let ((offset (symbol-value (jo/dtrt-offset-var))))
-  ;;   (setq tab-stop-list (number-sequence offset 180 offset))
-  ;; )
+  (jo--set-indent-vars offset tabs tabw)
+  (jo--on-indentation-changed)
+)
+
+(defun jo/auto-indentation ()
+  "Automagically setup indentation"
+  (interactive)
+  (when buffer-file-name
+
+      (jo--my-default-indentations-apply)
+
+      (require 'dtrt-indent)
+      (dtrt-indent-adapt)
+
+      (require 'editorconfig)
+      (editorconfig-mode-apply)
+
+      (jo--on-indentation-changed)
+    )
 )
 ;;(elp-instrument-function 'jo/default-indentation) ;; elp-results
 
 (dolist (hook '(change-major-mode-after-body-hook
                 read-only-mode-hook))
-  (add-hook hook 'jo/default-indentation))
-;;(add-hook 'text-mode-hook (lambda () (jo/default-indentation)))
-;;(add-hook 'prog-mode-hook (lambda () (jo/default-indentation)))
-
-(cl-defun jo/setup-indentation (offset tabs tabw &optional (ws '(face trailing indentation space-before-tab)))
-  "Setup indentation.
-
-Indentation of OFFSET width, using tabs of width TABW if TABS, and with WS whitespace-style (nil to disable)"
-  (interactive (list
-                (read-number (concat "Indent offset (" (symbol-name (jo/dtrt-offset-var)) ") ? ")
-                             (symbol-value (jo/dtrt-offset-var)))
-                (y-or-n-p (concat "Indent with tabs ? (default " (if indent-tabs-mode "y" "n")  ") "))
-                (read-number (concat "Tab width ? ") tab-width)
-                ))
-
-  (setq
-   indent-tabs-mode (or tabs)
-   tab-width        tabw
-   ;;standard-indent  offset
-   ;;c-basic-offset   offset
-   tab-stop-list    (number-sequence offset 180 offset)
-   )
-
-  (let ((offset-var (jo/dtrt-offset-var)))
-    (set offset-var offset)
-    (message "setup-indentation %s: tabs:%s tabw:%s %s:%s" major-mode indent-tabs-mode tab-width offset-var (symbol-value offset-var))
-    )
-
-  ;; (whitespace-mode 0)
-  ;; (when ws
-  ;;   (setq whitespace-style ws)
-  ;;   (whitespace-mode 1))
-)
+  (add-hook hook 'jo/auto-indentation))
+;;(add-hook 'text-mode-hook (lambda () (jo/auto-indentation))
+;;(add-hook 'prog-mode-hook (lambda () (jo/auto-indentation))
 
 (cl-defun jo/tab-space (&optional (offset 4))
   "Indent with 4 spaces."
   (interactive)
-  (jo/setup-indentation offset nil 8)
+  (jo/force-indentation offset nil 8)
   )
 
 (cl-defun jo/tab-tab (&optional (offset 4))
   "Indent with 1 tabulation of 4 spaces width."
   (interactive)
-  (jo/setup-indentation offset t offset)
+  (jo/force-indentation offset t offset)
   )
 
 (defun jo/tab-absurde ()
   "Indent absurde (4 spaces indent but replace 8 spaces by tabulation)"
   (interactive)
-  (jo/setup-indentation 4 t 8 '(face trailing))
+  (jo/force-indentation 4 t 8)
   )
 
 (defun jo/tab-term-8 ()
   "Indent 8 tab like a terminal."
   (interactive)
-  (jo/setup-indentation 8 t 8 nil)
+  (jo/force-indentation 8 t 8)
   )
 
 ;; https://stackoverflow.com/questions/11623721/can-i-just-tabify-begnning-of-lines-in-emacs
