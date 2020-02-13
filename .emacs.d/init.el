7b082f70a7d95f5c38ee0f32f6e24758d329d02f
Jordan Galby <gravemind2a@gmail.com>
2020-02-13 20:18:56 +0100

    emacs: helm-flex completion style

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2401,24 +2401,26 @@ many times might take a long time."
    ;;helm-idle-delay 0.01 ; be idle for this many seconds, before updating in delayed sources.
    ;;helm-input-idle-delay 0.01 ; be idle for this many seconds, before updating candidate buffer
 
-   ;; fuzzy matching everywhere
-   helm-mode-fuzzy-match t
-   helm-completion-in-region-fuzzy-match t
-   ;; fuzzy sort try preserve order
-   helm-fuzzy-sort-fn 'helm-fuzzy-matching-sort-fn-preserve-ties-order
+   completion-styles '(helm-flex)
+   helm-completion-styles '(helm-flex)
 
-   ;; Force fuzzy everywhere (helm-mode-fuzzy-match doesn't !?)
-   helm-ff-fuzzy-matching t
-   helm-recentf-fuzzy-match t
-   helm-buffers-fuzzy-matching t ;; not enabled by helm-mode-fuzzy-match !?
-   helm-locate-fuzzy-match t
-   helm-M-x-fuzzy-match t
-   helm-semantic-fuzzy-match t
-   helm-imenu-fuzzy-match t
-   helm-apropos-fuzzy-match t
-   helm-lisp-fuzzy-completion t
-   helm-session-fuzzy-match t
-   helm-etags-fuzzy-match t
+   ;; ;; fuzzy matching everywhere
+   ;; helm-mode-fuzzy-match t
+   ;; helm-completion-in-region-fuzzy-match t
+   ;; ;; fuzzy sort try preserve order
+   ;; helm-fuzzy-sort-fn 'helm-fuzzy-matching-sort-fn-preserve-ties-order
+   ;; ;; Force fuzzy everywhere (helm-mode-fuzzy-match doesn't !?)
+   ;; helm-ff-fuzzy-matching t
+   ;; helm-recentf-fuzzy-match t
+   ;; helm-buffers-fuzzy-matching t ;; not enabled by helm-mode-fuzzy-match !?
+   ;; helm-locate-fuzzy-match t
+   ;; helm-M-x-fuzzy-match t
+   ;; helm-semantic-fuzzy-match t
+   ;; helm-imenu-fuzzy-match t
+   ;; helm-apropos-fuzzy-match t
+   ;; helm-lisp-fuzzy-completion t
+   ;; helm-session-fuzzy-match t
+   ;; helm-etags-fuzzy-match t
 
    ;; Occur keep colors
    helm-moccur-show-buffer-fontification t
