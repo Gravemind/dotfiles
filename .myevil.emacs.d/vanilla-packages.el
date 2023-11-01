bd16d0979555f3b72e99bfb5a538cef3c83f14b9
Jordan Galby <gravemind2a@gmail.com>
2023-11-01 10:15:47 +0100

    emacs: Add vanilla-packages.el debug helper

---
+++ .myevil.emacs.d/vanilla-packages.el
@@ -0,0 +1,12 @@
+;;
+;; emacs -Q -l ~/.emacs.d/vanilla-packages.el
+;;
+
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(package-initialize)
