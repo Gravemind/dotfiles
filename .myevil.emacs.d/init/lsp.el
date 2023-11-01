8434ee2c606802dce494c689626a504bbb7dd070
Jordan Galby <gravemind2a@gmail.com>
2023-11-01 10:15:13 +0100

    emacs: Add lsp autoloads test

--- .myevil.emacs.d/init/lsp.el
+++ .myevil.emacs.d/init/lsp.el
@@ -8,9 +8,25 @@
          (lsp-mode . lsp-enable-which-key-integration)
          )
   :init
+  ;; (setq load-path (nconc load-path (list (concat my-packages-directory "/lsp-mode"))))
+  ;; (load-file (concat my-packages-directory "/lsp-autoloads.el"))
+
   ;; ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   ;; (setq lsp-keymap-prefix "C-c l")
 )
+
+;; ;; (use-package lsp :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-completion :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-diagnostics :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-headerline :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-icons :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-ido :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-iedit :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-lens :load-path (my-packages-directory "lsp-mode"))
+;; ;; (use-package lsp-mode :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-modeline :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-protocol :load-path (my-packages-directory "lsp-mode"))
+;; (use-package lsp-semantic-tokens :load-path (my-packages-directory "lsp-mode"))
 
 (use-package consult-lsp
   :load-path (my-packages-directory "consult-lsp")
