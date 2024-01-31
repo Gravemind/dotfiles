acd3f5cd7a63b10aef3dd657075b5ff16cc53b2f
Jordan Galby <gravemind2a@gmail.com>
2024-01-31 08:04:09 +0100

    emacs: Update better jumper config from doom emacs

--- .myevil.emacs.d/init/modes.el
+++ .myevil.emacs.d/init/modes.el
@@ -194,12 +194,7 @@
   ;;        ("C-o" . better-jumper-jump-backward)
   ;;        ("C-i" . better-jumper-jump-forward)
   ;;        )
-  :config
-
-  (global-set-key [remap evil-jump-forward]  #'better-jumper-jump-forward)
-  (global-set-key [remap evil-jump-backward] #'better-jumper-jump-backward)
-  (global-set-key [remap xref-pop-marker-stack] #'better-jumper-jump-backward)
-
+  :init
   (setq
    better-jumper-use-evil-jump-advice t
 
@@ -207,31 +202,39 @@
    better-jumper-context 'window
    better-jumper-new-window-behavior 'copy
    )
+  :config
 
-  (better-jumper-mode 1)
+  ;; See doom emacs
 
-  ;; Taken from doom-emacs
+  (global-set-key [remap evil-jump-forward]  #'better-jumper-jump-forward)
+  (global-set-key [remap evil-jump-backward] #'better-jumper-jump-backward)
+  (global-set-key [remap xref-pop-marker-stack] #'better-jumper-jump-backward)
+  (global-set-key [remap xref-go-back] #'better-jumper-jump-backward)
+  (global-set-key [remap xref-go-forward] #'better-jumper-jump-forward)
 
-  (defun doom-set-jump-a (orig-fn &rest args)
-    "Set a jump point and ensure ORIG-FN doesn't set any new jump points."
+  (defun doom-set-jump-a (fn &rest args)
+    "Set a jump point and ensure fn doesn't set any new jump points."
     (better-jumper-set-jump (if (markerp (car args)) (car args)))
     (let ((evil--jumps-jumping t)
           (better-jumper--jumping t))
-      (apply orig-fn args)))
+      (apply fn args)))
 
-  (defun doom-set-jump-maybe-a (orig-fn &rest args)
-    "Set a jump point if ORIG-FN returns non-nil."
+  (defun doom-set-jump-maybe-a (fn &rest args)
+    "Set a jump point if fn actually moves the point."
     (let ((origin (point-marker))
           (result
            (let* ((evil--jumps-jumping t)
                   (better-jumper--jumping t))
-             (apply orig-fn args))))
-      (unless result
+             (apply fn args)))
+          (dest (point-marker)))
+      (unless (equal origin dest)
         (with-current-buffer (marker-buffer origin)
           (better-jumper-set-jump
            (if (markerp (car args))
                (car args)
              origin))))
+      (set-marker origin nil)
+      (set-marker dest nil)
       result))
 
   (defun doom-set-jump-h ()
@@ -242,22 +245,22 @@
   ;; Creates a jump point before killing a buffer. This allows you to undo
   ;; killing a buffer easily (only works with file buffers though; it's not
   ;; possible to resurrect special buffers).
-  ;; (advice-add #'kill-current-buffer :around #'doom-set-jump-a)
+  ;;
+  ;; I'm not advising `kill-buffer' because I only want this to affect
+  ;; interactively killed buffers.
+  (advice-add #'kill-current-buffer :around #'doom-set-jump-a)
 
   ;; Create a jump point before jumping with imenu.
   (advice-add #'imenu :around #'doom-set-jump-a)
 
-  (when (eq my--compsys 'helm)
-    (add-hook 'helm-goto-line-before-hook
-              (lambda ()
-                (with-helm-current-buffer
-                  ;; Don't set-jump if we tab-tab-tab in helm result
-                  (unless helm-in-persistent-action
-                    (better-jumper-set-jump))
-                  )))
-    )
-
   (advice-add #'xref-push-marker-stack :around #'doom-set-jump-a)
+
+  ;; (advice-add #'find-function-do-it :around #'doom-set-jump-a)
+  ;; (advice-add #'find-file :around #'doom-set-jump-a)
+
+  (advice-add #'isearch-mode :around #'doom-set-jump-a)
+
+  (better-jumper-mode 1)
 )
 
 ;;
