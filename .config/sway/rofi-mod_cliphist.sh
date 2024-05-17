47d88e9db0d79dd86bfa20611ac163f5785f962a
Jordan Galby <gravemind2a@gmail.com>
2024-05-17 10:27:11 +0200

    rofi cliphist: Redirect messages to error output

--- .config/sway/rofi-mod_cliphist.sh
+++ .config/sway/rofi-mod_cliphist.sh
@@ -3,6 +3,8 @@
 if [[ -z "$1" ]]; then
     cliphist list
 else
-    # cliphist decode <<<"$1" | wl-copy
-    cliphist decode <<<"$1" | pee 'wl-copy' 'wl-copy --primary'
+    {
+        # cliphist decode <<<"$1" | wl-copy
+        cliphist decode <<<"$1" | pee 'wl-copy' 'wl-copy --primary'
+    } >&2
 fi
