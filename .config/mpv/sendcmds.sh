a9ba08f49a37e9104045bc5d112e6962a6309413
Jordan Galby <gravemind2a@gmail.com>
2024-06-02 09:37:35 +0200

    mpv: Add "sendcmds.sh command <cmd>..."

--- .config/mpv/sendcmds.sh
+++ .config/mpv/sendcmds.sh
@@ -3,8 +3,18 @@
 set -euo pipefail
 set -x
 
-for cmd in "$@"
-do
-    echo "$cmd" | socat - /tmp/mpvsocket
-    sleep 0.1
-done
+case "${1:-}"
+in
+    command)
+        shift 1
+        jq '{command: $ARGS.positional}' -n -c --args -- "$@" | tee /dev/stderr | socat - /tmp/mpvsocket
+        sleep 0.1
+        ;;
+    *)
+        for cmd in "$@"
+        do
+            echo "$cmd" | socat - /tmp/mpvsocket
+            sleep 0.1
+        done
+        ;;
+esac
