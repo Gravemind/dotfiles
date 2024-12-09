8439b0a357c65069926fa22df47d9187ccf74230
Jordan Galby <gravemind2a@gmail.com>
2024-12-09 22:22:14 +0100

    omz: Fix truncated man

--- .omz.custom/less.zsh
+++ .omz.custom/less.zsh
@@ -9,6 +9,10 @@ export LESS_TERMCAP_se=$'\e[0m'     # end standout-mode
 export LESS_TERMCAP_us=$'\e[01;36m' # begin underline (words)
 export LESS_TERMCAP_ue=$'\e[0m'     # end underline
 
+# Fix man colors on Konsole, Gnome-terminal, alacritty...
+export GROFF_NO_SGR=1  # all grotty
+# export MANROFFOPT=-P-c  # only man-pages
+
 export LESSBINFMT='*u<%02X>'
 
 # -q  : --quiet : less bell ringing
@@ -26,7 +30,11 @@ export LESSBINFMT='*u<%02X>'
 # -zn : --window=n : PAGE-up/down scrolled lines, negative for win_height-n
 export LESS='-qRiSM -j5 -z-4'
 
-export MANPAGER='less -J'
+function man() {
+	# `less -J` adds a 2 char-wide search result marker before each lines, so we
+	# need to reduce man's width to account for that.
+	MANPAGER="less -J" MANWIDTH="$((COLUMNS - 2))" command man "$@"
+}
 
 # for systemd (journalctl...)
 export SYSTEMD_LESS="$LESS -F"
