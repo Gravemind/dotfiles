a2b73090c8c2881a36fc2d2ead858939d1971f9f
Jordan Galby <gravemind2a@gmail.com>
2024-12-10 16:04:12 +0100

    emacs: eglot keep working after jump to header

--- .myevil.emacs.d/init/eglot.el
+++ .myevil.emacs.d/init/eglot.el
@@ -2,7 +2,13 @@
 (use-package eglot
   :load-path (my-packages-directory "eglot")
   :commands (eglot)
+
   :config
+  (setq-default
+   ;; When jumping across references, keep eglot connection from original source file.
+   ;; i.e. When jumping to an (external) header, keep asking LSP from the original .cpp.
+   eglot-extend-to-xref t
+   )
 
   (add-to-list 'eglot-server-programs
                `((c-mode c-ts-mode c++-mode c++-ts-mode) . ,(eglot-alternatives '(
