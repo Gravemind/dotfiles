320463ba4b5357284ce0bdee6f887d254ef834fc
Jordan Galby <gravemind2a@gmail.com>
2025-03-23 16:52:43 +0100

    emacs: Eglot use latest clangd available

--- .myevil.emacs.d/init/eglot.el
+++ .myevil.emacs.d/init/eglot.el
@@ -3,4 +3,14 @@
   :load-path (my-packages-directory "eglot")
   :commands (eglot)
   :config
+
+  (add-to-list 'eglot-server-programs
+               `((c-mode c-ts-mode c++-mode c++-ts-mode) . ,(eglot-alternatives '(
+                                 ("clangd-20")
+                                 ("clangd-19")
+                                 ("clangd-18")
+                                 ("clangd-17")
+                                 ("clangd-16")
+                                 ("clangd-15")
+                                 ))))
 )
