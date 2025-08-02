e8d780c902b8596d22518b6b434c877fa3f2b1d1
Jordan Galby <gravemind2a@gmail.com>
2025-08-02 12:39:05 +0200

    omz: change my systemctl aliases

--- .omz.custom/systemd.zsh
+++ .omz.custom/systemd.zsh
@@ -2,14 +2,12 @@
 # see .oh-my-zsh/plugins/systemd/systemd.plugin.zsh for sudo or not sudo
 
 __systemctl_journal() {
-	local sudo=
-	[[ -z "$1" ]] || sudo="$1"
+	local sudo="${1:-}"
 	shift
-
 	$sudo echo "$fg_bold[blue]Begin $sudo systemctl $@ & ...$reset_color"
 	begin="$(date +'%F %T')"
 	{
-		$sudo systemctl "$@"
+		$sudo systemctl --no-pager "$@"
 		r="$?"
 		if [[ "$r" -ne 0 ]] ; then
 			echo "$fg_bold[blue]... End, $sudo systemctl $@ & $fg_bold[red]failed ($r)$fg_bold[blue] (Ctrl-C to quit)$reset_color"
@@ -17,7 +15,6 @@ __systemctl_journal() {
 			echo "$fg_bold[blue]... End, $sudo systemctl $@ & $fg_bold[green]succeeded$fg_bold[blue] (Ctrl-C to quit)$reset_color"
 		fi
 	} &
-	sleep 0.2
 	journalctl -f --since "$begin"
 }
 _systemctl_journal() {
@@ -29,16 +26,23 @@ _sudo_systemctl_journal() {
 }
 compdef _sudo_systemctl_journal='systemctl'
 
-alias sc='systemctl'
+alias sdr='sudo systemctl daemon-reload'
+alias sdru='systemctl --user daemon-reload'
 
-alias journalu='journalctl -eu'
+alias sd='sudo systemctl'
+alias sdu='systemctl --user'
 
-alias start='sudo systemctl start'
-alias stop='sudo systemctl stop'
-alias restart='sudo systemctl restart'
-alias reload='sudo systemctl reload'
-alias status='systemctl status'
+alias sdj='_sudo_systemctl_journal'
+alias sduj='_systemctl_journal --user'
 
-alias startj='_sudo_systemctl_journal start'
-alias restartj='_sudo_systemctl_journal restart'
-alias reloadj='_sudo_systemctl_journal reload'
+# alias journalu='journalctl -eu'
+
+# alias start='sudo systemctl start'
+# alias stop='sudo systemctl stop'
+# alias restart='sudo systemctl restart'
+# alias reload='sudo systemctl reload'
+# alias status='systemctl status'
+
+# alias startj='_sudo_systemctl_journal start'
+# alias restartj='_sudo_systemctl_journal restart'
+# alias reloadj='_sudo_systemctl_journal reload'
