a034de822166ad26a68a4756066df6207a3056f9
Jordan Galby <gravemind2a@gmail.com>
2026-01-16 00:01:34 +0100

    mpv: snappier fast-forward, remember pause state

--- .config/mpv/scripts/fast-forward.js
+++ .config/mpv/scripts/fast-forward.js
@@ -1,19 +1,26 @@
 
 var ff = 20;
+var paused = true;
 
 function fast_forward(data) {
     if (data.event == "down") {
-        mp.command("set play-dir forward; set speed " + ff)
+        paused = mp.get_property("pause")
+        mp.command("set play-direction forward; set speed " + ff + "; set pause no;")
     } else if (data.event == "up") {
-        mp.command("set play-dir forward; set speed 1")
+        mp.command("set play-direction forward; set speed 1; set pause " + paused)
+        // Fix playback continuing fast forward/backward for a bit
+        mp.command("seek 0 relative")
     }
 }
 
 function fast_backward(data) {
     if (data.event == "down") {
-        mp.command("set play-dir backward; set speed " + ff)
+        paused = mp.get_property("pause")
+        mp.command("set play-direction backward; set speed " + ff + "; set pause no;")
     } else if (data.event == "up") {
-        mp.command("set play-dir forward; set speed 1")
+        mp.command("set play-direction forward; set speed 1; set pause " + paused)
+        // Fix playback continuing fast forward/backward for a bit
+        mp.command("seek 0 relative")
     }
 }
 
