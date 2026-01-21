72ac64509cd4cc271dc07d11713c3edc5986b7d0
Jordan Galby <gravemind2a@gmail.com>
2026-01-21 12:13:57 +0100

    omz fzf: add live command output widget

--- .omz.custom/fzf.zsh
+++ .omz.custom/fzf.zsh
@@ -20,3 +20,26 @@ zle     -N             fzf-history-arg-widget
 bindkey -M emacs '\er' fzf-history-arg-widget
 bindkey -M vicmd '\er' fzf-history-arg-widget
 bindkey -M viins '\er' fzf-history-arg-widget
+
+#
+# Run the current prompt with live output in fzf
+#
+# Alt-I
+#
+fzf-live-preview-widget() {
+	local prompt="$BUFFER"
+	local shell="${(qqq)SHELL} -c {q}"
+	local newprompt
+	newprompt="$(:| $(__fzfcmd) --print-query --multi --preview="$shell" --preview-window=down:99% --query="$prompt" --prompt="$SHELL > ")"
+	local r=$?
+	if [[ $r -eq 0 || $r -eq 1 ]]
+	then
+		BUFFER="$newprompt"
+		CURSOR=$#BUFFER
+	fi
+	return 0
+}
+zle     -N             fzf-live-preview-widget
+bindkey -M emacs '\ei' fzf-live-preview-widget
+bindkey -M vicmd '\ei' fzf-live-preview-widget
+bindkey -M viins '\ei' fzf-live-preview-widget
