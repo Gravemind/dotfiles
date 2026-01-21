5c61980d171646d0f6ecb76e3755ed3bb41c1c10
Jordan Galby <gravemind2a@gmail.com>
2026-01-21 12:13:32 +0100

    omz fzf: tweak search arg

--- .omz.custom/fzf.zsh
+++ .omz.custom/fzf.zsh
@@ -1,14 +1,18 @@
+#!/bin/zsh
 
-# ATL-R - Paste the selected command *argument* from history into the command line
+#
+# Search and insert a previous argument from history
+#
+# Alt-R
+#
 # FIXME: \n in arguments
 fzf-history-arg-widget() {
   local selected
-  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases noglob nobash_rematch 2> /dev/null
   selected="$({echo $LBUFFER; fc -rnl 1;} | while read -r l; do args=(${(z)l}); printf '%s\n' "${(Oa)args[@]}"; done | awk 'length($0) <= 2 {next;} { if(!seen[$0]++) print $0 }' |
 	FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_ALT_R_OPTS-} +m") \
 	FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd))"
   local ret=$?
-  LBUFFER="${LBUFFER% } ${selected% }"
+  LBUFFER="${LBUFFER% } ${selected% } "
   zle reset-prompt
   return $ret
 }
