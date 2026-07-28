68016873317cdcf60c2cbe88aad95160f52fdbd6
Jordan Galby <gravemind2a@gmail.com>
2026-07-28 14:46:14 +0200

    omz: fix binding for tmux

--- .omz.custom/key-bindings.zsh
+++ .omz.custom/key-bindings.zsh
@@ -60,7 +60,7 @@ do
 	# Enable Ctrl-Backspace only on supported term
 	case "$TERM"
 	in
-		 rxvt*|xterm*|alacritty|foot)
+		 rxvt*|xterm*|alacritty|foot|tmux*)
 			 bindkey -M $b "^H" backward-delete-word  # [Ctrl-Backspace]
 			 ;;
 	esac
